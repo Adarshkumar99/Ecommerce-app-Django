88d521d48a9f83b0be056c8ef0642a201a467b60
Adarsh Kumar <adarshrajputhjp@gmail.com>
2026-03-23 13:07:07 +0100

    Fix whitenoise storage

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
